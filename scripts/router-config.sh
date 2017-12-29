d2796bbaeb5527f9ee866f2f2cb155cb8ff58fd8
Robin Roelandt <roelandt.robin@gmail.com>
2017-12-29 14:52:03 +0100

    basis router

--- scripts/router-config.sh
+++ scripts/router-config.sh
@@ -10,24 +10,39 @@ delete system console device ttyS0
 # Basic settings
 #
 set system host-name 'Router'
-#set service ssh port '22'
+
+set service ssh port '22'
 
 #
 # IP settings
 #
 
 # TODO
+set interfaces ethernet eth0 address dhcp
+set interfaces ethernet eth0 description 'WAN link'
 
+set interfaces ethernet eth1 address '192.0.2.254/24'
+set interfaces ethernet eth1 description 'DMZ'
+
+set interfaces ethernet eth2 address '172.16.255.254/16'
+set interfaces ethernet eth2 description 'internal'
 #
 # Network Address Translation
 #
 
 # TODO
+set nat source rule 100 outbound-interface 'eth0'
+set nat source rule 100 source address '192.0.2.0/24'
+set nat source rule 100 translation address masquerade
 
 #
 # Time
 #
 
+set system ntp server '0.be.pool.ntp.org'
+set system ntp server '1.be.pool.ntp.org'
+set system time-zone 'UTC'
+#time zone nog aanpassen!!!
 #
 
 #
@@ -36,6 +51,10 @@ set system host-name 'Router'
 
 # TODO
 
+set service dns forwarding cache-size '0'
+set service dns forwarding listen-on 'eth1'
+set service dns forwarding listen-on 'eth2'
+set service dns forwarding name-server '192.0.2.10'
 # Make configuration changes persistent
 commit
 save
