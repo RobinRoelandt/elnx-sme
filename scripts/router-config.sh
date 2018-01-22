8898520ae1427d3af876fe697002fcb02b2822d7
Robin Roelandt <roelandt.robin@gmail.com>
2018-01-22 18:00:47 +0100

    fix voor internet

--- scripts/router-config.sh
+++ scripts/router-config.sh
@@ -26,7 +26,6 @@ set interfaces ethernet eth1 description 'DMZ'
 set interfaces ethernet eth2 address 172.16.255.254/16
 set interfaces ethernet eth2 description 'internal'
 
-set system gateway-address 10.0.2.15
 #
 # Network Address Translation
 #
@@ -39,10 +38,6 @@ set nat source rule 100 translation address 'masquerade'
 set nat source rule 101 outbound-interface 'eth1'
 set nat source rule 101 source address '172.16.0.0/16'
 set nat source rule 101 translation address 'masquerade'
-
-set nat source rule 200 outbound-interface 'eth0'
-set nat source rule 200 source address '192.0.2.0/24'
-set nat source rule 200 translation address 'masquerade'
 
 #
 # Time
@@ -60,8 +55,10 @@ set system time-zone 'Europe/Brussels'
 
 # TODO
 set service dns forwarding domain avalon.lan server 192.0.2.10
-set service dns forwarding name-server 8.8.8.8 #193.190.173.1  # DNS server van Hogent of GOOGLE DNS OM THUIS TE TESTEN
+set service dns forwarding dhcp 'eth0'
+set service dns forwarding listen-on 'eth1'
 set service dns forwarding listen-on 'eth2'
+
 # Make configuration changes persistent
 commit
 save
